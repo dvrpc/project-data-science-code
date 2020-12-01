41e830658dbd6920f9e47139730fa5f1516e68ed
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-01 10:17:38 -0500

    initial project setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='project_data_science_code',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Project-specific data science code',
+    author='Aaron Fraint, AICP',
+    license='MIT',
+    entry_points="""
+        [console_scripts]
+        ucity=fy21_university_city.cli:main
+    """,
+)
