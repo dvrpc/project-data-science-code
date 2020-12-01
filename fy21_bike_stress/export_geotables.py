a5130c2cbf1d453ca439cf9501c536258fc376d4
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-01 10:29:15 -0500

    add comment structure

--- fy21_bike_stress/export_geotables.py
+++ fy21_bike_stress/export_geotables.py
@@ -7,43 +7,58 @@ import psycopg2
 from pathlib import Path
 from geopandas import GeoDataFrame
 
-# Change these parameters to point towards your database
+# Step 1: Change these parameters to point towards your database
+# --------------------------------------------------------------
 UN = "postgres"
 PW = "password"
 HOST = "localhost"
 PORT = 5432
 DB_NAME = "ucity"
 
-# Define where you want to save the resulting shapefiles
-SHP_FOLDER = Path(".")
-
-
 URI = f"postgresql://{UN}:{PW}@{HOST}:{PORT}/{DB_NAME}"
 
-
-# Get a list of all spatial tables
-sql_all_spatial_tables = """
-    SELECT f_table_name AS tblname
-    FROM geometry_columns
-"""
-
-connection = psycopg2.connect(URI)
-cursor = connection.cursor()
-cursor.execute(sql_all_spatial_tables)
-
-spatial_tables = cursor.fetchall()
-
-cursor.close()
-connection.close()
+# Step 2: Define where you want to save the resulting shapefiles
+# --------------------------------------------------------------
+SHP_FOLDER = Path("/Users/aaron/data")
 
 
-# Iterate over the list of spatial tables, save each to shapefile
+# Step 3A: Manually define a list of tables you want to export
+#          To do so, update the first "table_list" and
+#                    remove the 2nd one that is set to None
+# ------------------------------------------------------------
+
+table_list = ["first_table", "second_table"]
+table_list = None
+
+
+# Step 3B: Get a list of all spatial tables in the database
+#          if none is defined
+# ----------------------------------------------------------
+
+if not table_list:
+
+    sql_all_spatial_tables = """
+        SELECT f_table_name AS tblname
+        FROM geometry_columns
+    """
+
+    connection = psycopg2.connect(URI)
+    cursor = connection.cursor()
+    cursor.execute(sql_all_spatial_tables)
+
+    spatial_tables = cursor.fetchall()
+    tables = [x[0] for x in spatial_tables]
+
+    cursor.close()
+    connection.close()
+
+
+# Step 4: Iterate over the list of spatial tables, save each to shapefile
+# -----------------------------------------------------------------------
 
 print(f"Saving {len(spatial_tables)} spatial tables to shapefile")
 
-for table in spatial_tables:
-
-    table = table[0]
+for table in tables:
 
     print("\t-> Saving", table)
     query = f"""
