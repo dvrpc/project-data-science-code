449f55a53fe983b2de440fc91eec7b0d16ae3e7e
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-02 17:01:22 -0500

    use next trip to get the end time of the trip

--- fy21_university_city/process_hts_data.py
+++ fy21_university_city/process_hts_data.py
@@ -3,30 +3,31 @@ Use the 2012-13 Household Travel Survey data
 
 """
 from datetime import time
+from tqdm import tqdm
+
 from fy21_university_city.db_io import db_connection
 
-mode_codes = {
-    1:  "Walk",
-    2:  "Bike",
-    3:  "Private Vehicle",
-    4:  "Private Transit",
-    5:  "Public Transit",
-    6:  "School Bus",
-    7:  "Other"
-}
+# mode_codes = {
+#     1:  "Walk",
+#     2:  "Bike",
+#     3:  "Private Vehicle",
+#     4:  "Private Transit",
+#     5:  "Public Transit",
+#     6:  "School Bus",
+#     7:  "Other"
+# }
 
 db = db_connection()
 
 # Get a basic table out of SQL
 query = """
-    select o_cpa, d_cpa, mode_agg, arrive, depart, compositeweight
+    select person_id, trip_num, act_dur1, o_cpa, d_cpa, mode_agg, arrive, depart, compositeweight
     from hts_2013_trips
+    order by person_id, trip_num
 """
 
 df = db.query_as_df(query)
 
-df["o_time"] = "_"
-df["d_time"] = "_"
 df["time_class"] = "_"
 
 
@@ -45,30 +46,77 @@ def timeify(txt: str) -> time:
 
 
 def time_is_between(the_time: time, start: int, end: int) -> bool:
-    return time(start) <= the_time < time(end):
+    return time(start) <= the_time < time(end)
+
 
 def is_am_peak(the_time: time) -> bool:
     return time_is_between(the_time, 6, 10)
 
+
 def is_pm_peak(the_time: time) -> bool:
-    return time_is_between(the_time, 6, 10)
+    return time_is_between(the_time, 15, 19)
+
+
+def find_next_trip(person_id, trip_num):
+
+    query = f"""
+        SELECT arrive
+        FROM hts_2013_trips
+        WHERE
+            person_id = {person_id}
+          AND
+            trip_num > {trip_num}
+        ORDER BY trip_num ASC
+        LIMIT 1
+    """
+
+    return db.query_as_list(query)
 
 
 for idx, row in df.iterrows():
-    o = classify_time_value(row.depart)
-    d = classify_time_value(row.arrive)
+
+    time_at_origin = row.depart
+    time_at_destination = find_next_trip(row.person_id, row.trip_num)
+
+    o = classify_time_value(time_at_origin)
+    d = classify_time_value(time_at_destination)
+
+    time_class = "..."
+
+
+    print(row.person_id, row.trip_num, row.act_dur1, time_at_origin, time_at_destination)
 
     # If both times are known...
     if o == "Usable data" and d == "Usable data":
-        o_time = timeify(row.depart)
-        d_time = timeify(row.arrive)
+        o_time = timeify(time_at_origin)
+        d_time = timeify(time_at_destination)
 
-        if time(6) <= o_time < time(10):
-            print("starts in AM peak")
-        if time(6) <= o_time < time(10):
-            print("ends in AM peak")
+        if is_am_peak(o_time) or is_am_peak(d_time):
+            time_class = "AM period"
 
+        if is_pm_peak(o_time) or is_pm_peak(d_time):
+            time_class = "PM period"
 
     # If only depart time is known...
+    elif o == "Usable data":
+        o_time = timeify(time_at_origin)
+
+        if is_am_peak(o_time):
+            time_class = "AM period"
+
+        if is_pm_peak(o_time):
+            time_class = "PM period"
 
     # If only arrive time is known...
+    elif d == "Usable data":
+        d_time = timeify(time_at_destination)
+
+        if is_am_peak(d_time):
+            time_class = "AM period"
+
+        if is_pm_peak(d_time):
+            time_class = "PM period"
+
+    df.at[idx, "time_class"] = time_class
+
+db.import_dataframe(df, "hts_2013_trips_processed", if_exists="replace")
