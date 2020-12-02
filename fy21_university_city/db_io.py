2cae5a9744bf4d1ac9eed48f095624e6a0457019
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-02 14:56:33 -0500

    check if data exists in db before importing

--- fy21_university_city/db_io.py
+++ fy21_university_city/db_io.py
@@ -6,9 +6,7 @@ from postgis_helpers import PostgreSQL
 
 from philly_transit_data import TransitData
 
-DB_NAME = "ucity"
-
-GDRIVE_FOLDER = Path("/Volumes/GoogleDrive/Shared drives/U_City_FY_21")
+from fy21_university_city import DB_NAME, GDRIVE_FOLDER
 
 
 def db_connection(db_name: str = DB_NAME) -> PostgreSQL:
@@ -19,20 +17,31 @@ def setup(db_name: str = DB_NAME):
 
     db = db_connection(db_name)
 
+    all_tables = db.all_tables_as_list()
+
     # Load APC data
-    apc_csv = GDRIVE_FOLDER / "Data/2019_Spring_APC_Stop_by_Route.csv"
-    df = pd.read_csv(apc_csv)
-    df.drop(columns=["Unnamed: 0"], inplace=True)
-    db.import_dataframe(df, "apc_raw", if_exists="replace")
+    if "apc_raw" not in all_tables:
+        apc_csv = GDRIVE_FOLDER / "Data/2019_Spring_APC_Stop_by_Route.csv"
+        df = pd.read_csv(apc_csv)
+        df.drop(columns=["Unnamed: 0"], inplace=True)
+        db.import_dataframe(df, "apc_raw", if_exists="replace")
 
     # Load regional transit stops
-    transit_data = TransitData()
-    stops, lines = transit_data.all_spatial_data()
-    db.import_geodataframe(stops, "transit_stops")
+    if "transit_stops" not in all_tables:
+        transit_data = TransitData()
+        stops, lines = transit_data.all_spatial_data()
+        db.import_geodataframe(stops, "transit_stops")
 
     # Load study area bounds
-    bounds_shp = GDRIVE_FOLDER / "Data/GIS/Draft_Study_Area_Extent/U_CIty_Study_Area_Dissolve_2.shp"
-    db.import_geodata("study_bounds", bounds_shp)
+    if "study_bounds" not in all_tables:
+        bounds_shp = GDRIVE_FOLDER / "Data/GIS/Draft_Study_Area_Extent/U_CIty_Study_Area_Dissolve_2.shp"
+        db.import_geodata("study_bounds", bounds_shp)
+
+    # Load the 2013 HTS Trip table
+    if "hts_2013_trips" not in all_tables:
+        hts_xlsx = GDRIVE_FOLDER / "Data/PublicUse/export_from_access_db_4_Trip_Public.xlsx"
+        df = pd.read_excel(hts_xlsx)
+        db.import_dataframe(df, "hts_2013_trips")
 
 
 def export_shp(table_to_export: str, db_name: str = DB_NAME):
