d2b795b5828b87ae20bb5405066d807824beb9c5
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-03 14:40:24 -0500

    add windows GDrive path

--- fy21_university_city/__init__.py
+++ fy21_university_city/__init__.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import platform
 
 DB_NAME = "ucity"
-GDRIVE_FOLDER = Path("/Volumes/GoogleDrive/Shared drives/U_City_FY_21")
+
+if platform.system() == "Darwin":
+    GDRIVE_FOLDER = Path("/Volumes/GoogleDrive/Shared drives/U_City_FY_21")
+
+elif platform.system() == "Windows":
+    GDRIVE_FOLDER = Path(r"G:\Shared drives\U_City_FY_21")
