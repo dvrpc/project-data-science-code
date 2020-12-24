3a48e98e7a70fcd2b0a30217bf64439522b261cf
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-12-24 14:27:03 -0500

    make copy of data to edit

--- fy21_downingtown/cli.py
+++ fy21_downingtown/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from fy21_downingtown import db_io
+from fy21_downingtown.db_io import db_connection
 
 
 @click.group()
@@ -15,8 +16,30 @@ def setup_db():
     db_io.setup()
 
 
+@click.command()
+def prep_local_osm_layer():
+    """Clip Downingtown roads for MANUAL edits """
+    db = db_connection()
+
+    db.make_geotable_from_query(
+        """
+            SELECT *
+            FROM osm_edges
+            WHERE st_intersects(geom, (SELECT geom FROM boundary))
+        """,
+        "osm_downingtown",
+        "LINESTRING",
+        26918,
+    )
+
+    db.table_add_or_nullify_column("osm_downingtown", "travel_lanes", "FLOAT")
+    db.table_add_or_nullify_column("osm_downingtown", "parking_lanes", "TEXT")
+    db.table_add_or_nullify_column("osm_downingtown", "design_plan", "TEXT")
+
+
 all_commands = [
     setup_db,
+    prep_local_osm_layer,
 ]
 
 for cmd in all_commands:
