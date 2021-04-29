e6da2f0baf60efa888d75244492f67587d8010d7
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-04-29 16:24:56 -0400

    get data and attributes necessary for study

--- fy21_university_city/process_crash_data.py
+++ fy21_university_city/process_crash_data.py
@@ -1,18 +1,63 @@
-"""
-Aggregate collisions within the study area
-"""
 import pg_data_etl as pg
 
 from fy21_university_city import GDRIVE_FOLDER
 
+db = pg.Database("ucity", **pg.connections()["localhost"])
 
-def main(db: pg.Database):
+GIS_FOLDER = GDRIVE_FOLDER / "Data/GIS"
 
-    if "public.pa_crashes" not in db.spatial_table_list():
-        shp_filepath = GDRIVE_FOLDER / "Data/GIS/PA Crashes/PA_Crashes.shp"
-        db.shp2pgsql(shp_filepath, 4326, "pa_crashes")
+
+def _load_data():
+    """
+    - Load crash points and tabular FLAG data
+    """
+
+    if "public.philly_crashes" not in db.spatial_table_list():
+        filepath = GIS_FOLDER / "philly_crashes.geojson"
+        db.import_geo_file(filepath, "philly_crashes")
+
+    if "public.pa_crash_flags" not in db.table_list():
+        filepath = GIS_FOLDER / "crash_pa_flag_202104291218.csv"
+        db.import_tabular_file(filepath, "philly_crash_flags")
+
+
+def main():
+    """
+    - Load data into SQL
+    - Query the points/attributes relevant to this project
+    - Save result to shapefile
+    """
+
+    _load_data()
+
+    query = """
+        select
+            c.crn,
+            c.crash_year,
+            b.study_area as ucity_zone,
+            f.fatal_or_maj_inj as ksi,
+            f.bicycle as bike,
+            f.pedestrian as ped,
+            f.non_interstate,
+            c.geom 
+        from philly_crashes c
+        left join
+            philly_crash_flags f
+            on f.crn = c.crn
+        right join
+            study_bounds b 
+            on st_intersects(
+                st_transform(c.geom, 26918),
+                b.geom
+            )
+        where c.crash_year != '2014'
+    """
+    db.make_geotable_from_query(query, "study_area_crashes", "POINT", 4326)
+
+    output_filepath = GIS_FOLDER / "study_area_crashes_with_flags.shp"
+
+    db.ogr2ogr_export("study_area_crashes", output_filepath)
 
 
 if __name__ == "__main__":
-    db = pg.Database("ucity", **pg.connections()["localhost"])
-    main(db)
+    main()
