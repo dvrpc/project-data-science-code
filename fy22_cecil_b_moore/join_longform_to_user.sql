d7879325b247e1cf52d2836c0f811c8ed6f7165a
mmorley <mmorley0395@gmail.com>
2021-12-29 13:33:40 -0500

    updated table name and changed q5 to qaqc'd column

--- fy22_cecil_b_moore/join_longform_to_user.sql
+++ fy22_cecil_b_moore/join_longform_to_user.sql
@@ -1,5 +1,5 @@
-create table longform_joined as (
-select users.ip_address, users.responded_to_survey_question, users.q1, users.q2, users.q3, users.q4, users.q5, users.q6, users.q7, longform."usage", longform.frequency, longform."mode", longform.mode_issues, longform.condition_1, longform.condition_2, longform.condition_3, longform.condition_4, longform.condition_5, longform.condition_6, longform.condition_7, longform.condition_8, longform.condition_9, longform.created_on 
+create table longform_online as (
+select users.ip_address, users.responded_to_survey_question, users.q1, users.q2, users.q3, users.q4, users.q5_gender, users.q6, users.q7, longform."usage", longform.frequency, longform."mode", longform.mode_issues, longform.condition_1, longform.condition_2, longform.condition_3, longform.condition_4, longform.condition_5, longform.condition_6, longform.condition_7, longform.condition_8, longform.condition_9, longform.ideas, longform.priorities, longform.created_on 
 from pins_longformsurvey as longform
 inner join pins_mapuser as users
 on users.ip_address = longform.ip_address )
