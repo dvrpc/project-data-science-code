47a9b20f9daf340486583138d6f3d11880cb6628
mmorley <mmorley0395@gmail.com>
2021-12-29 13:30:54 -0500

    counts zip code and joins with zip pasda shp

---
+++ fy22_cecil_b_moore/zips_with_counts_shp.sql
@@ -0,0 +1,6 @@
+create table zips_with_counts as (
+	select zcta5ce, geom, count(lj.q6)
+	from tl_pennsylvania5digit2009 tpd 
+	inner join longform_joined lj 
+	on lj.q6 = tpd.zcta5ce 
+	group by zcta5ce, tpd.geom)
