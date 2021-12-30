cffeb8645663fc1dfc153174ce26eec2cdcc1ba2
mmorley <mmorley0395@gmail.com>
2021-12-29 19:13:33 -0500

    added new query for top priority

--- fy22_cecil_b_moore/priorities.sql
+++ fy22_cecil_b_moore/priorities.sql
@@ -3,4 +3,11 @@ SELECT split_part(priorities, ';', 1) AS p1
      , split_part(priorities, ';', 3) AS p3
      , split_part(priorities, ';', 4) AS p4
      , split_part(priorities, ';', 5) AS p5
-FROM   longform_joined lj ;
+FROM   longform_joined lj;
+
+
+SELECT split_part(priorities, ';', 1) as p1, count(priorities) as count
+from longform_joined lj 
+group by p1
+
+
