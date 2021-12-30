1679217c80b8219226cbec61839ddf08346e64f9
mmorley <mmorley0395@gmail.com>
2021-12-30 13:01:29 -0500

    deleted and replaced

--- fy22_cecil_b_moore/nonstudent.sql
+++
@@ -1,6 +0,0 @@
-with nonstudent as(
-select q2, q3, q4, q5_gender, q6, q7, "usage", frequency, mode from longform_joined lj 
-where "usage" not like '%Commute to school%')
-select q2, count(q2) from nonstudent 
-group by q2
-
