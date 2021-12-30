25128dd3bf8bc6a0ffcc5b08cf3cec5e8491ca0e
mmorley <mmorley0395@gmail.com>
2021-12-30 11:25:41 -0500

    added drop statement

--- fy22_cecil_b_moore/zips_with_counts_shp.sql
+++ fy22_cecil_b_moore/zips_with_counts_shp.sql
@@ -1,6 +1,9 @@
+drop table if exists zips_with_counts;
+
 create table zips_with_counts as (
 	select zcta5ce, geom, count(lj.q6)
 	from tl_pennsylvania5digit2009 tpd 
 	inner join longform_joined lj 
 	on lj.q6 = tpd.zcta5ce 
 	group by zcta5ce, tpd.geom)
+	
