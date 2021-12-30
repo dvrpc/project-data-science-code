e153bbe5629a9c5ed3625c7646810bdbafbf68e7
mmorley <mmorley0395@gmail.com>
2021-12-30 13:01:04 -0500

    added student and nonstudent priorities

---
+++ fy22_cecil_b_moore/student_nonstudent_priorities.sql
@@ -0,0 +1,15 @@
+with nonstudent as(
+select q2, q3, q4, q5_gender, q6, q7, "usage", frequency, priorities from longform_joined lj 
+where "usage" not like '%Commute to school%')
+SELECT split_part(priorities, ';', 1) as p1, count(priorities) as p1count
+from nonstudent 
+group by p1
+order by p1
+
+with student as(
+select q2, q3, q4, q5_gender, q6, q7, "usage", frequency, priorities from longform_joined lj 
+where "usage" like '%Commute to school%')
+SELECT split_part(priorities, ';', 1) as p1, count(priorities) as p1count
+from student
+group by p1
+order by p1
