c73ffa405ad921fb2a4ce3dc986f3ed93ab586dd
mmorley <mmorley0395@gmail.com>
2021-12-30 11:26:03 -0500

    updated with new queries

--- fy22_cecil_b_moore/race_breakdown.sql
+++ fy22_cecil_b_moore/race_breakdown.sql
@@ -1,2 +1,9 @@
+select q2, count(q2) from longform_online lo 
+group by q2
+
+select q2, count(q2) from  in_person_survey_response_data_entry ipsrde 
+group by q2
+
 select q2, count(q2) from longform_joined lj 
 group by q2
+
