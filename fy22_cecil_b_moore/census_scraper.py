8fbbae7ce8a522272ee734522e71c451f4fd6244
Mark Morley <mmorley0395@gmail.com>
2022-02-14 10:33:24 -0500

    successfully pulls from API. now to optimize!

--- fy22_cecil_b_moore/census_scraper.py
+++ fy22_cecil_b_moore/census_scraper.py
@@ -2,11 +2,10 @@
 import pandas as pd
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 api_key = os.getenv("CENSUS_KEY")  # put census key from census api in env file
-trait = "B02001_001E,B02001_002E,B02001_003E,B02001_004E,B02001_005E,B02001_006E,B02001_007E,B02001_007E"  # Eventually refactor into a legible dictionary for common queries
+trait = "B02001_001E,B02001_002E,B02001_003E,B02001_004E,B02001_005E,B02001_006E,B02001_007E,B02001_008E" #Eventually refactor into a legible dictionary for common queries
 zip = "19121"
 state = "42"  # PA, swap for 34 if looking at NJ
 year = "2019"
@@ -18,19 +17,14 @@ df = pd.read_json(url)
 new_header = df.iloc[0]  # grab the first row for the header
 df = df[1:]  # take the data less the header row
 df.columns = new_header  # set the header row as the df header
-vartable = pd.read_json(
-    "https://api.census.gov/data/2019/acs/acs5/profile/variables.json"
-)  # lookup table
-vartable.columns = ["code"]
-for t in trait_list:
-    print(vartable.code.DP02PR_0001E["label"])
-
-df.head()
-
-
-vartable = pd.read_json(
-    "https://api.census.gov/data/2019/acs/acs5/profile/variables.json"
-)
-vartable.columns = ["code"]
-print(vartable.code.DP02PR_0001E["label"])
-vartable.head(20)
+var_lookup = pd.read_json(
+    "https://api.census.gov/data/2019/acs/acs5/variables.json"
+)  # lookup table to make headers legible by referencing data dictionary
+var_lookup.columns = ["code"]
+new_column_list=[]
+for item in trait_list:
+    if item in var_lookup.code:
+        raw_col = var_lookup.code[item].get('label')
+        clean_col = raw_col.replace("Estimate!!Total:!!", "")
+        df.rename(columns= {item : clean_col}, inplace=True)
+df.head(20)
