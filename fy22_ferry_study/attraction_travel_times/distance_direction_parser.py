a2df575ccdf4bffe595566738a9aafece9ad3668
mmorley <mmorley0395@gmail.com>
2022-02-28 16:02:37 -0500

    further cleanup, added transit

--- fy22_ferry_study/attraction_travel_times/distance_direction_parser.py
+++ fy22_ferry_study/attraction_travel_times/distance_direction_parser.py
@@ -14,7 +14,7 @@ destinations = GDRIVE_FOLDER / "attractions.csv"
 origin_df = pd.read_csv(origins)
 destinations_df = pd.read_csv(destinations)
 now = datetime.now()
-list_of_dicts = []
+
 
 origin = (
     39.946196,
@@ -23,22 +23,25 @@ origin = (
 mode = "driving"  # accepts driving, walking, transit, cycling
 
 
-def distance_duration(destination):
-    """measures distance and duration between two points.
-    in this case my origin is always the same so i've defined it separately above.
-    appends the returned dictionary to a list of dictionaries."""
-
-    matrix = gmaps.directions(
-        origin,
-        destination,
-        mode,
-        units="imperial",
-        departure_time=now,
-    )
-    list_of_dicts.append(matrix)
-
-
 def distance_duration_iteration():
+    driving_list = []
+    transit_list = []
+
+    def distance_duration(destination):
+        output = gmaps.directions(
+            origin,
+            destination,
+            mode,
+            units="imperial",
+            departure_time=now,
+        )
+
+        if mode == "driving":
+            driving_list.append(output)
+
+        if mode == "transit":
+            transit_list.append(output)
+
     """loops through the destinations file and runs the distance duration function"""
     for idx, row in destinations_df.iterrows():
         lat = row["Latitude"]
@@ -46,26 +49,31 @@ def distance_duration_iteration():
         destination = (lat, lon)
         distance_duration(destination)
 
+    if mode == "driving":
+        return driving_list
+    if mode == "transit":
+        return transit_list
 
-def unpack_dicts(output):
+
+def unpack_dicts(list_of_dicts):
     """function to crack into the nested dictionary structure that the api returns"""
-    labels = ["distance", "duration", "duration_in_traffic", "lat", "lng"]
+    labels = ["distance", "duration", "lat", "lng"]
     df = pd.DataFrame(columns=labels)
+
     for item in list_of_dicts:
         destination_lat = item[0]["legs"][0]["end_location"]["lat"]
         destination_lng = item[0]["legs"][0]["end_location"]["lng"]
         distance = item[0]["legs"][0]["distance"]["text"]
         duration = item[0]["legs"][0]["duration"]["text"]
-        duration_in_traffic = item[0]["legs"][0]["duration_in_traffic"]["text"]
         d = {
             "distance": [distance],
             "duration": [duration],
-            "duration_in_traffic": [duration_in_traffic],
             "lat": [destination_lat],
             "lng": [destination_lng],
         }
         list_df = pd.DataFrame(data=d)
         df = pd.concat([list_df, df], axis=0, ignore_index=True)
+
     return df
 
 
@@ -73,11 +81,5 @@ def df_to_csv(df):
     df.to_csv(GDRIVE_FOLDER / f"{mode}_travel_times.csv", sep=",")
 
 
-if __name__ == "__main__":
-    distance_duration_iteration()
-    df_to_csv(unpack_dicts(list_of_dicts))
-
-
-# todo: add target datetime variable instead of just using datetimen now
-# add functionality to discern between transit/driving, perhaps return both in one dataframe
-# add fare functionality
+if __name__ == "main":
+    df_to_csv(unpack_dicts(distance_duration_iteration()))
