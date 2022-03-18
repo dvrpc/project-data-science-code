78e09c12093dacf1b3a6b1c2c3596af05be0baa3
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2022-03-18 14:58:02 -0400

    update handling of 'None' values and str to number type conversion

--- fy22_delco_trails/score_trailheads.py
+++ fy22_delco_trails/score_trailheads.py
@@ -1,23 +1,27 @@
+from pathlib import Path
 import geopandas as gpd
 
+data_folder = Path(
+    r"G:\Shared drives\FY22 Delaware County Bicycle Network Planning\Prioritization Analysis\analysis outputs"
+)
+
 # Read the geojson file with the query results
-gdf = gpd.read_file("G:\Shared drives\FY22 Delaware County Bicycle Network Planning\Prioritization Analysis\analysis outputs\results.geojson")
+gdf = gpd.read_file(data_folder / "results.geojson")
 
 # Make new columns for all the scores, setting them all to zero by defauly
-gdf["score_votes"] = 0
-gdf["score_population"] = 0
-gdf["score_park"] = 0
-gdf["score_ipd"] = 0
-gdf["score_employment"] = 0
-gdf["score_job"] = 0
-gdf["score_destination"] = 0
-gdf["score_activitycenter"] = 0
-gdf["score_food"] = 0
-gdf["score_health"] = 0
-gdf["score_college"] = 0
-gdf["score_school"] = 0
-gdf["score_transit"] = 0
-
+gdf["s_votes"] = 0
+gdf["s_population"] = 0
+gdf["s_park"] = 0
+gdf["s_ipd"] = 0
+gdf["s_employment"] = 0
+gdf["s_job"] = 0
+gdf["s_destination"] = 0
+gdf["s_activitycenter"] = 0
+gdf["s_food"] = 0
+gdf["s_health"] = 0
+gdf["s_college"] = 0
+gdf["s_school"] = 0
+gdf["s_transit"] = 0
 
 
 # Iterate over every row in the dataset
@@ -39,11 +43,11 @@ for idx, row in gdf.iterrows():
         vote_score = 5
 
     # C: add the score to the row in the new column
-    gdf.at[idx, "score_votes"] = vote_score
+    gdf.at[idx, "s_votes"] = vote_score
 
     # Population
     # ----------
-    pop = row["population"]
+    pop = float(row["population"])
     if pop > 80000:
         pop_score = 3
     elif 30000 < pop <= 80000:
@@ -51,156 +55,177 @@ for idx, row in gdf.iterrows():
     elif 15000 < pop <= 30000:
         pop_score = 1
 
-    gdf.at[idx, "score_population"] = pop_score
+    gdf.at[idx, "s_population"] = pop_score
 
-    # TODO:
     # Park distance
-    park = row["park_dist"]
+    park = float(row["park_dist"])
 
     if park == 0:
-        park_score =  3
-    elif 1 <= park <= 200:
+        park_score = 3
+    elif 0 < park <= 200:
         park_score = 2
     elif 200 < park <= 800:
         park_score = 1
 
-    gdf.at[idx, "score_park"] = park_score
+    gdf.at[idx, "s_park"] = park_score
 
-    # IPD 
+    # IPD
     ipd = row["ipd_class"]
 
     if ipd == "Well Above Average":
-        ipd_score =  7
+        ipd_score = 7
     elif ipd == "Above Average":
         ipd_score = 5
     elif ipd == "Average":
         ipd_score = 3
-    elif ipd == "Well Below Average":
-        ipd_score = 1
-    elif ipd == "Below Average":
+    elif ipd in ["Well Below Average", "Below Average"]:
         ipd_score = 1
 
-    gdf.at[idx, "score_ipd"] = ipd_score
+    gdf.at[idx, "s_ipd"] = ipd_score
 
     # Employment
-    emp = row["num_employers"]
+    emp = float(row["num_employers"])
 
     if emp > 3000:
-        emp_score =  3
+        emp_score = 3
     elif 2000 < emp <= 3000:
         emp_score = 2
     elif 500 < emp <= 2000:
         emp_score = 1
 
-    gdf.at[idx, "score_employment"] = emp_score
+    gdf.at[idx, "s_employment"] = emp_score
 
     # Jobs
-    jobs = row["num_jobs"]
+    jobs = float(row["num_jobs"])
 
     if jobs > 15000:
-        jobs_score =  3
+        jobs_score = 3
     elif 10000 < jobs <= 15000:
         jobs_score = 2
     elif 5000 < jobs <= 10000:
         jobs_score = 1
 
-
-    gdf.at[idx, "score_jobs"] = jobs_score
+    gdf.at[idx, "s_jobs"] = jobs_score
 
     # Destinations
-    dest = row["number_of_destination_types"]
+    dest = float(row["number_of_destination_types"])
 
     if 5 <= dest <= 6:
-        dest_score =  3
+        dest_score = 3
     elif 3 <= dest <= 4:
         dest_score = 2
     elif 1 <= dest <= 2:
         dest_score = 1
 
-
-    gdf.at[idx, "score_destination"] = dest_score
+    gdf.at[idx, "s_destination"] = dest_score
 
     # Activity centers for seniors or disabled
 
     act = row["places_activitycenterforseniorsordisabled"]
+    if act is None:
+        act = 0
+    else:
+        act = float(act)
 
     if 8 <= act <= 10:
-        act_score =  3
+        act_score = 3
     elif 4 <= act <= 7:
         act_score = 2
     elif 1 <= act <= 3:
         act_score = 1
 
+    gdf.at[idx, "s_activitycenter"] = act_score
 
-    gdf.at[idx, "score_activitycenter"] = act_score
-
-    # Food 
+    # Food
 
     food = row["places_foodstore"]
+    if food is None:
+        food = 0
+    else:
+        food = float(food)
 
     if food > 34:
-        food_score =  3
+        food_score = 3
     elif 10 < food <= 34:
         food_score = 2
     elif 1 <= food <= 10:
         food_score = 1
+    else:
+        food_score = 0
 
-
-    gdf.at[idx, "score_food"] = food_score
+    gdf.at[idx, "s_food"] = food_score
 
     # Health
-   
+
     health = row["places_healthfacility"]
+    if health is None:
+        health = 0
+    else:
+        health = float(health)
 
     if health > 9:
-        health_score =  3
+        health_score = 3
     elif 4 < food <= 9:
         health_score = 2
     elif 1 <= health <= 4:
         health_score = 1
+    else:
+        health_score = 0
 
+    gdf.at[idx, "s_health"] = health_score
 
-    gdf.at[idx, "score_health"] = health_score
-
-    # Colleges - This might not be the best way to handle it.
+    # Colleges
     college = row["places_school_college_university"]
-    if 1 <= college <= 4:
-        college_score = 4
+    college_score = 0
 
-    gdf.at[idx, "score_college"] = college_score 
+    if college:
+        if float(college) >= 1:
+            college_score = 4
 
-    # Public and private schools - how do I referenced the sum of both fields?
-    school = row["places_school"]
+    gdf.at[idx, "s_college"] = college_score
 
+    # Public and private schools
+    public_school = row["places_school_public"]
+    private_school = row["places_school_private"]
+
+    if public_school is None:
+        public_school = 0
+    if private_school is None:
+        private_school = 0
+
+    school = float(public_school) + float(private_school)
     if school > 9:
-        school_score =  5
+        school_score = 5
     elif 8 <= school <= 9:
         school_score = 4
     elif 5 <= school <= 7:
         school_score = 3
     elif 3 <= school <= 4:
-        school_score= 2
+        school_score = 2
     elif 1 <= school <= 2:
-        school_score= 1
-       
-    gdf.at[idx, "score_school"] = school_score 
-    
+        school_score = 1
+
+    gdf.at[idx, "s_school"] = school_score
+
     # Transit
 
-    transit = row["number_of_transit_routes"]
+    transit = float(row["number_of_transit_routes"])
 
     if transit > 12:
-       transit_score =  5
+        transit_score = 5
     elif 10 <= transit <= 12:
         transit_score = 4
     elif 8 <= transit <= 9:
         transit_score = 3
     elif 4 <= transit <= 7:
-        transit_score= 2
+        transit_score = 2
     elif 1 <= transit <= 3:
-        transit_score= 1
-       
-    gdf.at[idx, "score_transit"] = transit_score 
+        transit_score = 1
+
+    gdf.at[idx, "s_transit"] = transit_score
 
 # After iterating over all rows, write to new file
-gdf.to_file("./results_scored.geojson")
+gdf.to_file(data_folder / "results_scored.geojson", driver="GeoJSON")
+
+df = gdf.drop(columns=["geometry"])
+df.to_csv(data_folder / "scored_results_table.csv")
